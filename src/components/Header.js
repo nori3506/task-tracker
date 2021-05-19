6d19628cb6451048c0766da0f1455073f3f4bbd1
Nori <loveandfight2186@yahoo.co.jp>
2021-05-19 16:26:52 -0700

    hide add button in About page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom'
 import Button from './button'
 
 const Header = ({ title, onAdd, showAdd }) => {
- 
+  const location = useLocation()
   return (
     <header className='header'>
       <h1> {title} </h1>
-      <Button onClick={onAdd} color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} />
+      {location.pathname === '/' && (
+        <Button onClick={onAdd} color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} />
+      )}
     </header>
   )
 }
-
 
 Header.defaultProps = {
   title: "task Tracker"
@@ -19,9 +21,5 @@ Header.defaultProps = {
 Header.propTypes = {
   title: PropTypes.string.isRequired,
 }
-
-// const headingStyle = {
-//   color: 'red', backgroundColor: 'black'
-// }
 
 export default Header
